e324b354c292f6c1263d207725ffef31c7dc8733
hexeva <patrick79.g@gmail.com>
2021-12-03 16:33:40 +0100

    active image thumb

--- js/script.js
+++ js/script.js
@@ -1,58 +1,46 @@
 Vue.config.devtools = true;
 
 
-const app = new Vue(
-
-    {
+const app = new Vue({
         el:'#root',
         data:{
             carouselSlides:[
                 {
-                    imgList:'img/01.jpg',
+                    image:'img/01.jpg',
                     name:'Svezia',
                     description:'Ma quanto è bella la Svezia'
             
                 },
                 {
-                    imgList:'img/02.jpg',
+                    image:'img/02.jpg',
                     name:'Svizzera',
                     description:'La Svizzera e le meraviglie dei suoi paesaggi montani'
             
                 },
                 {
-                    imgList:'img/03.jpg',
+                    image:'img/03.jpg',
                     name:'Gran Bretagna',
                     description:'I suggestivi paesaggi della Gran Bretagna'
             
                 },
                 {
-                    imgList:'img/04.jpg',
+                    image:'img/04.jpg',
                     name:'Germania',
                     description:'Il fascino delle foreste tedesche e dei suoi castelli'
             
                 },
                 {
-                    imgList:'img/05.jpg',
+                    image:'img/05.jpg',
                     name:'Paradise',
                     description:'Le magnifiche spiagge di Paradise'
             
-                }
-                
-           
+                }   
             ],
-            
-            
-
-
+            activeImg:0,
          },
         // end data
         methods:{
-            myFunction:function(){
-                
-                alert('test');
-            }
+            
         },
-        created:{},
-    }
-
-);
+        
+});
